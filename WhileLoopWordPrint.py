74ffcdc500d5eaaa33a2dd6d9b0e0ca843ea1926
Arpit-Agrawal-27 <arpitagrawal404@gmail.com>
2024-09-16 22:58:28 +0530

    Create WhileLoopWordPrint.py
    Program for printing a string with the help of while loop in horizontal way

---
+++ WhileLoopWordPrint.py
@@ -0,0 +1,6 @@
+#Program for Print Word With the help of While loop
+word=input("Enter the Word:")
+index=0 #Initilization
+while index<len(word): #condition
+    print(word[index])
+    index+=1 # Updation
