ea309a02b80c1e76cf91e85c292184b33e800994
Arpit-Agrawal-27 <arpitagrawal404@gmail.com>
2024-09-16 23:13:48 +0530

    WhileLoopWordPrint

--- WhileLoopWordPrint.py
+++ WhileLoopWordPrint.py
@@ -1,6 +1,17 @@
 #Program for Print Word With the help of While loop
 word=input("Enter the Word:")
+print("-"*50)
+print("Word in FordWard Direction")
+print("-"*50)
 index=0 #Initilization
 while index<len(word): #condition
     print(word[index])
     index+=1 # Updation
+else:
+    print("-"*50)
+    print("Word in backward direction")
+    print("-" * 50)
+    index=-1 #Initilization
+    while index>=-len(word): #condition
+        print(word[index])
+        index=index-1 #
