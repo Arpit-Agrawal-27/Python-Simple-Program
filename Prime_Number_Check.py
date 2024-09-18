926b60528da8e5934cee1ac5c6fe0a3655ff155d
Arpit-Agrawal-27 <arpitagrawal404@gmail.com>
2024-09-18 11:26:30 +0530

    Create Prime_Number_Check.py
    #Program For to Check The Given Number Is Prime Or Not

---
+++ Prime_Number_Check.py
@@ -0,0 +1,18 @@
+#Program for to decide The Given Number Prime or Not
+n=int(input("Enter the Number:"))
+res = "Prime Number"
+if n<=1:
+    print("{} is Invalid Input".format(n))
+else:
+
+    i=2
+    while i<n:
+        if n%i==0:
+            res="Not Prime Number"
+            break
+        i+=1
+print()
+print("-"*50)
+print("{} is {}".format(n,res))
+print("-"*50)
+
