8eb9d437b606368a9cc115a32b214667c6e1033f
Arpit-Agrawal-27 <arpitagrawal404@gmail.com>
2024-09-25 07:25:28 +0530

    Update Si.py

--- Si.py
+++ Si.py
@@ -1,18 +1,20 @@
 #Program for to create the function for calculating the simple interest and total amount
 def simple_interest():
-    p=int(input())
-    t=int(input())
-    r=int(input())
+    p=int(input("Enter the principal Amount:"))
+    t=int(input("Enter the Time:"))
+    r=int(input("Enter the Rate of Intrest"))
     si=(p*t*r)/100
     totalamu=si+p
     return p,t,r,si,totalamu
 #function call
 res=simple_interest()
-print("-"*50)
-print(res[0])
-print(res[1])
-print(res[2])
-print(res[3])
-print(res[4])
-
+   print("*"*50)
+    print("Calculations of Simple Interest")
+    print("*"*50)
+    print("\tPrinciple Amount:{}".format(res[0]))
+    print("\tTime:{}".format(res[1]))
+    print("\tRate of Interest:{}".format(res[2]))
+    print("\tSimple Interest:{}".format(res[3]))
+    print("\tTotal Amount to Pay:{}".format(res[4]))
+    print("*"*50)
   
